ebfd396f922998a4e35424575cd2c2adcdbb5cdf
Alparslan Tarkan <atarkan@hotmail.com>
2024-10-22 01:58:29 +0200

    implemented while loop for input validation

--- app/src/main/java/com/example/rockpaperscissors/RockPaperScissors.kt
+++ app/src/main/java/com/example/rockpaperscissors/RockPaperScissors.kt
@@ -3,29 +3,39 @@ package com.example.rockpaperscissors
 fun main(){
     var computerChoice = ""
     var playerChoice = ""
-    println("Rock, Paper or Scissors? Enter your choice!")
-    playerChoice = readln()
+
+    while (true) {
+        println("Rock, Paper, or Scissors? Enter your choice!")
+        playerChoice = readln().lowercase()
+
+        if (playerChoice == "rock" || playerChoice == "paper" || playerChoice == "scissors") {
+            break
+        } else {
+            println("Invalid choice. Please enter 'rock', 'paper', or 'scissors'.")
+        }
+    }
 
     val randomNumber = (1..3).random()
 
-    when (randomNumber){
-        1 -> {
-        computerChoice = "Rock"
-    }
-        2 -> {
-        computerChoice = "Paper"
-    }
-        3 -> {
-        computerChoice = "Scissors"
+    when (randomNumber) {
+        1 -> computerChoice = "rock"
+        2 -> computerChoice = "paper"
+        3 -> computerChoice = "scissors"
     }
 
-}
-    println(computerChoice)
-    val winner = when{
+    println("Computer chose: $computerChoice")
+
+    val winner = when {
         playerChoice == computerChoice -> "Tie"
-        playerChoice == "Rock" && computerChoice == "Scissors" -> "Player"
-        playerChoice == "Paper" && computerChoice == "Rock" -> "Player"
-        playerChoice == "Scissors" && computerChoice == "Paper" -> "Player"
+        playerChoice == "rock" && computerChoice == "scissors" -> "Player"
+        playerChoice == "paper" && computerChoice == "rock" -> "Player"
+        playerChoice == "scissors" && computerChoice == "paper" -> "Player"
         else -> "Computer"
+    }
+
+    if (winner == "Tie") {
+        println("It's a tie")
+    } else {
+        println("$winner won!")
     }
 }
