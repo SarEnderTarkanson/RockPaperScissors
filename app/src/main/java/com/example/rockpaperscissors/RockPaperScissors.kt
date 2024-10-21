bf77fcb608f67b05632e85d031c7c1dbf357161e
Alparslan Tarkan <atarkan@hotmail.com>
2024-10-22 01:25:36 +0200

    implemented simple logic to find out the winner

--- app/src/main/java/com/example/rockpaperscissors/RockPaperScissors.kt
+++ app/src/main/java/com/example/rockpaperscissors/RockPaperScissors.kt
@@ -8,14 +8,24 @@ fun main(){
 
     val randomNumber = (1..3).random()
 
-    if (randomNumber == 1){
+    when (randomNumber){
+        1 -> {
         computerChoice = "Rock"
     }
-    else if (randomNumber == 2){
+        2 -> {
         computerChoice = "Paper"
     }
-    else if (randomNumber == 3){
+        3 -> {
         computerChoice = "Scissors"
     }
+
+}
     println(computerChoice)
+    val winner = when{
+        playerChoice == computerChoice -> "Tie"
+        playerChoice == "Rock" && computerChoice == "Scissors" -> "Player"
+        playerChoice == "Paper" && computerChoice == "Rock" -> "Player"
+        playerChoice == "Scissors" && computerChoice == "Paper" -> "Player"
+        else -> "Computer"
+    }
 }
